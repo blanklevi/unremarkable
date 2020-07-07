1c54bf0e9f98772e0b8e3383cfbeb8c3d2e7e80b
LEVI ____ <blanklevi@gmail.com>
2020-07-07 14:06:50 -0700

    cart functionality works

--- Shop/urls.py
+++ Shop/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.index, name="shop"),
     path('cart', views.cart_page, name="cart"),
     path('checkout', views.checkout_page, name="checkout"),
+    path('update_item', views.updateItem, name="update_item"),
+    path('process_order', views.processOrder, name="process_order"),
 ]
--- Shop/views.py
+++ Shop/views.py
@@ -1,15 +1,32 @@
 from django.shortcuts import render, redirect
+from django.http import JsonResponse
 from .models import *
 from Users.models import *
+import json
+import datetime
 
 def index(request):
     # User info
     if request.session['log_email'] == []:
+        # logged_user = request.session['log_email']
         logged_user = []
         sign_in_button = ["Levi is the best"]
+        items = []
+        order = {
+            'get_cart_total': 0,
+            'get_cart_items': 0,
+            'shipping': False,
+        }
+        cartItems = order['get_cart_items']
     else:
         logged_user = request.session['log_email']
-        sign_in_button = [] 
+        sign_in_button = []
+        user = User.objects.filter(email=logged_user)
+        for u in user:
+            customer = u.customer  
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all() 
+        cartItems = order.get_cart_items
     
     # Shop info
     products = Product.objects.all()
@@ -18,6 +35,7 @@ def index(request):
         'logged_user': logged_user,
         'sign_in_button': sign_in_button,
         'products': products,
+        'cartItems': cartItems,
     }
     return render(request, "Shop/index.html", context)
 
@@ -28,7 +46,10 @@ def cart_page(request):
         order = {
             'get_cart_total': 0,
             'get_cart_items': 0,
+            'shipping': False,
         }
+        logged_user = request.session['log_email']
+        cartItems = order['get_cart_items']
     else:
         logged_user = request.session['log_email']
         user = User.objects.filter(email=logged_user)
@@ -36,13 +57,50 @@ def cart_page(request):
             customer = u.customer  
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
     context = {
         'items': items,
         'order': order,
+        'logged_user': logged_user,
+        'cartItems': cartItems,
     }
     return render(request, "Shop/cart.html", context)
 
 def checkout_page(request):
+    # This is a ratchet workaround for "is someone logged in?"
+    if request.session['log_email'] == []:
+        logged_user = request.session['log_email']
+        items = []
+        order = {
+            'get_cart_total': 0,
+            'get_cart_items': 0,
+            'shipping': False,
+        }
+        cartItems = order['get_cart_items']
+    else:
+        logged_user = request.session['log_email']
+        user = User.objects.filter(email=logged_user)
+        for u in user:
+            customer = u.customer  
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
+    context = {
+        'items': items,
+        'order': order,
+        'cartItems': cartItems,
+        'logged_user': logged_user,
+    }
+    return render(request, "Shop/checkout.html", context)
+
+def updateItem(request):
+    data = json.loads(request.body)
+    productId = data['productId']
+    action = data['action']
+
+    print('action:', action)
+    print('productId:', productId)
+    
     # This is a ratchet workaround for "is someone logged in?"
     if request.session['log_email'] == []:
         items = []
@@ -50,15 +108,48 @@ def checkout_page(request):
             'get_cart_total': 0,
             'get_cart_items': 0,
         }
+        cartItems = order['get_cart_items']
     else:
         logged_user = request.session['log_email']
         user = User.objects.filter(email=logged_user)
         for u in user:
             customer = u.customer  
+        # customer = request.user.customer
+        product = Product.objects.get(id=productId)
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
-        items = order.orderitem_set.all()
-    context = {
-        'items': items,
-        'order': order,
-    }
-    return render(request, "Shop/checkout.html", context)
+        orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+    
+    if action == 'add':
+        orderItem.quantity = (orderItem.quantity + 1)
+    elif action == 'remove':
+        orderItem.quantity = (orderItem.quantity - 1)
+    orderItem.save()
+    if orderItem.quantity <= 0:
+        orderItem.delete()
+
+    return JsonResponse('Item was added', safe=False)
+
+def processOrder(request):
+    transaction_id = datetime.datetime.now().timestamp()
+    data = json.loads(request.body)
+    
+    if request.session['log_email'] == []:
+        print("User is not logged in currently")
+    else:
+        logged_user = request.session['log_email']
+        user = User.objects.filter(email=logged_user)
+        for u in user:
+            customer = u.customer 
+        order, created = Order.objects.get_or_create(customer=customer, complete=False) 
+        total = float(data['form']['total'])
+        order.transaction_id = transaction_id
+
+        # Protect from hax
+        if total == order.get_cart_total:
+            order.complete = True
+        order.save()
+        
+        # If it is not a digital product
+        if order.shipping == True:
+            ShippingAddress.objects.create(customer=customer, order=order, address=data['shipping']['address'], city=data['shipping']['city'], state=data['shipping']['state'], zipcode=data['shipping']['zipcode'])
+    return JsonResponse('Payment Completed', safe=False)
