df53482cae306f3573eaa54dfcfb4c7e43953fce
LEVI ____ <blanklevi@gmail.com>
2020-07-22 11:10:58 -0700

    working on sizing

--- Shop/models.py
+++ Shop/models.py
@@ -20,7 +20,6 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=9, decimal_places=2)
     digital = models.BooleanField(default=False, null=True, blank=True)
     image = models.ImageField(null=True)
-    size = models.CharField(max_length=20, null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -67,6 +66,7 @@ class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
     quantity = models.IntegerField(default=0, null=True, blank=True)
+    size = models.CharField(max_length=25)
     date_added = models.DateTimeField(auto_now_add=True)
 
     @property
